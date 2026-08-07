0361d66f91e14b4ec3882240822a8d82fad0dfd7
archivebtw <archivebtw@gmail.com>
2026-08-07 19:42:50 +0300

    feat: auto-detect proposal player counts

--- assets/js/public/figma-ui.js
+++ assets/js/public/figma-ui.js
@@ -2,7 +2,7 @@
   const assets = [
     ['style','figmaMainFinalStyles','./assets/css/public/figma-main-final.css?v=1.0'],
     ['style','adminFigmaFinalStyles','./assets/css/public/admin-figma-final.css?v=1.0'],
-    ['style','proposalWindowsFigmaStyles','./assets/css/public/proposal-windows-figma.css?v=4.4'],
+    ['style','proposalWindowsFigmaStyles','./assets/css/public/proposal-windows-figma.css?v=4.5'],
     ['style','proposalMediaFigmaExactStyles','./assets/css/public/proposal-media-figma-exact.css?v=2.1'],
     ['style','adminCardIconsStyles','./assets/css/public/admin-card-icons.css?v=1.1'],
     ['script','proposalWindowsFigmaScript','./assets/js/public/proposal-windows-figma.js?v=3.4'],
